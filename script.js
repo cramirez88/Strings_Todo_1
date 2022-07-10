71ab7f235c384d69d43f325c7eaae1f6bf923b25
Christian Ramirez <christian.r0602@gmail.com>
2022-07-10 10:56:04 -0500

    getDigits

--- script.js
+++ script.js
@@ -21,3 +21,31 @@
 // }
 
 // console.log(removeBlanks('H e l  lo'))
+
+
+// Get Digits
+// Create a JavaScript function that given a string, returns the integer made from the string’s digits. You are allowed to use isNaN() and Number().
+
+// Examples:
+
+// getDigits("abc8c0d1ngd0j0!8") => 801008
+
+// getDigits("0s1a3y5w7h9a2t4?6!8?0") => 1357924680
+
+
+// function getDigits(str){
+//   let digitString = ''
+//   // loop through string
+//   for (let i = 0; i < str.length; i++){
+//      // if character in string is an integer add it to a new string
+//      if( !isNaN(str[i])){
+//       digitString += str[i]
+//      }
+//   }
+//   // return new string
+//   return digitString
+ 
+  
+// }
+
+// console.log(getDigits('nn12njn3n69f5'))
