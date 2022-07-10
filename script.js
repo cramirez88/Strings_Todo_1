5bebe85cb1c4558f26db0179fbd3618032a49259
Christian Ramirez <christian.r0602@gmail.com>
2022-07-10 10:45:28 -0500

    removeBlanks

---
+++ script.js
@@ -0,0 +1,23 @@
+// Strings To Do 1
+// Write functions using the JavaScript language for all challenges. Use only a single JavaScript file for this assignment. Do not use built-in methods (unless otherwise instructed). (.length is a property, and is allowed.)
+
+// Remove Blanks
+// Create a function that, given a string, returns all of that string’s contents, but without blanks. 
+
+// Examples:
+
+// removeBlanks(" Pl ayTha tF u nkyM usi c ") => "PlayThatFunkyMusic"
+// removeBlanks("I can not BELIEVE it's not BUTTER") => "IcannotBELIEVEit'snotBUTTER"
+
+
+// function removeBlanks(str){
+//   let newString = ''
+//   for(let i = 0; i < str.length; i++){
+//     if(str[i] !== ' '){
+//       newString += str[i]
+//     }
+//   }
+// return newString
+// }
+
+// console.log(removeBlanks('H e l  lo'))
